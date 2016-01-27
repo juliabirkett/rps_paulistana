279351a68d015a67bb3d9e0e95955ff3ed1a5847
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-01-27 16:15:46 -0200

    Adding implementation to Register class.

--- lib/register.rb
+++ lib/register.rb
@@ -1,12 +1,25 @@
 module NFE
     class Register
-        VALID_TYPES = [1, 2, 3, 5, 9]
+        VALID_TYPES     = [1, 2, 3, 5, 9]
+        REQUIRED_FIELDS = []
+
+        def validate!
+            if !@type.is_a? String and !@type.is_a? Integer
+                raise Errors::ParamClassError,      /Parameter type must be String or Integer/
+            end
+
+            if !VALID_TYPES.include?(@type.to_i)
+                raise Errors::RPSRegisterTypeError, /Invalid RPS Register type/
+            end
+        end
 
         def initialize type
-            raise ValidationError if !VALID_TYPES.include?(type.to_i) 
             @type = type
+            validate!
+        end
+
+        def valid?
+            return (REQUIRED_FIELDS - @fields.keys).empty?
         end
     end
-
-    class ValidationError < StandardError; end
 end
