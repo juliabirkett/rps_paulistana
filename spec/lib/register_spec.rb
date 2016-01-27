3091515ee22d1089ad05f3cb9887845b7ce60f84
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-01-27 16:17:15 -0200

    Improving Register class specs.

--- spec/lib/register_spec.rb
+++ spec/lib/register_spec.rb
@@ -4,20 +4,26 @@ describe NFE::Register do
     describe "#new" do
         context "with no parameters or more than one parameter" do
             it "raises an ArgumentError" do
-                expect{ NFE::Register.new }.to                   raise_error ArgumentError
-                expect{ NFE::Register.new "first", "second" }.to raise_error ArgumentError
+                expect{ NFE::Register.new }.to                        raise_error ArgumentError
+                expect{ NFE::Register.new String.new, String.new }.to raise_error ArgumentError
             end
         end
 
-        context "with an invalid register type" do
-            it "raises ValidationError" do
-                expect{ NFE::Register.new "99" }.to raise_error NFE::ValidationError
+        context "with an invalid RPS Register Type" do
+            it "raises RPSRegisterTypeError" do
+                expect{ NFE::Register.new "99" }.to raise_error NFE::Errors::RPSRegisterTypeError, /Invalid RPS Register type/
             end
         end
 
-        context "with a valid register type" do
-            it "return NFE::Register object" do
-                expect(NFE::Register.new "1").to be_an_instance_of NFE::Register
+        context "with an invalid parameter type class" do
+            it "raises ParamClassError" do
+                expect{ NFE::Register.new :param }.to raise_error NFE::Errors::ParamClassError, /Parameter type must be String or Integer/
+            end
+        end
+
+        context "with a valid RPS Register type" do
+            it "returns a NFE::Register object" do
+                expect(NFE::Register.new "1").to be_an NFE::Register
             end
         end
     end
