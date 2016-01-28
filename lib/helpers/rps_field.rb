23f2b337e9d126cde75f102ea6844ceba9ff6b35
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-01-28 18:20:26 -0200

    Adding fill_with! method implementation.

--- lib/helpers/rps_field.rb
+++ lib/helpers/rps_field.rb
@@ -19,17 +19,29 @@ module NFE
 
             def valid?
                 case @name.to_sym
+                when :taker_email
+                    self.fill_with! " ", 75
+                when :taker_document
+                    self.fill_with! "0", 14 and self.numeric?
+                when :taker_name
+                    self.fill_with! " ", 75 and self.alphanumeric?
+                when :address_type
+                    self.fill_with! " ", 3 and self.alphanumeric?
+                when :address, :city
+                    self.fill_with! " ", 50 and self.alphanumeric?
+                when :address_number, :tributary_source
+                    self.fill_with! " ", 10 and self.alphanumeric?
+                when :complement, :district
+                    self.fill_with! " ", 30 and self.alphanumeric?
+                when :rps_type, :rps_serial
+                    self.fill_with! " ", 5 and self.alphanumeric?
                 when :layout_version
                     self.numeric? and self.length == 3
                 when :municipal_registration, :date, :taker_ccm, :zip_code
                     self.numeric? and self.length == 8
-                when :rps_type
-                    self.alphanumeric? and self.length == 5
-                when :rps_number
+                when :rps_number, :cei
                     self.numeric? and self.length == 12
-                when :rps_status
-                    self.alphanumeric? and self.length == 1
-                when :service_code
+                when :service_code, :tributary_percentage
                     self.numeric? and self.length == 5
                 when :amount
                     self.numeric? and self.length == 15
@@ -37,27 +49,19 @@ module NFE
                     self.numeric? and self.length == 4
                 when :iss_by, :taker_type
                     self.numeric? and self.length == 1
-                when :taker_document
-                    self.numeric? and self.length == 14
-                when :taker_name, :taker_email
-                    self.alphanumeric? and self.length == 75
-                when :address_type
-                    self.alphanumeric? and self.length == 3
-                when :address, :city
-                    self.alphanumeric? and self.length == 50
-                when :address_number
-                    self.alphanumeric? and self.length == 10
-                when :complement, :district
-                    self.alphanumeric? and self.length == 30
+                when :total_detail_lines, :city_ibge_code
+                    self.numeric? and self.length == 7
+                when :rps_status
+                    self.alphanumeric? and self.length == 1
                 when :uf
                     self.alphanumeric? and self.length == 2
-                when :total_detail_lines
-                    self.numeric? and self.length == 7
+                when :service_description
+                    self.alphanumeric?
                 end
             end
 
             def alphanumeric?
-                return @value.match(/^[[:alnum:]]+$/) != nil
+                return @value.match(/^[[:alnum:]\s]+$/) != nil
             end
 
             def numeric?
@@ -66,6 +70,14 @@ module NFE
 
             def length
                 return @value.length
+            end
+
+            def fill_with! char, size
+                while self.length < size do
+                    @value << char
+                end
+
+                return self.length == size
             end
         end
     end
