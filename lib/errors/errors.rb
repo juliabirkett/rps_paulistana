ba2658565d3bbe70c32c8d1ba6a85a00191b4500
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-01-28 18:17:58 -0200

    Adding InvalidParamError

--- lib/errors/errors.rb
+++ lib/errors/errors.rb
@@ -2,5 +2,6 @@ module NFE
     module Errors
         class ParamClassError      < StandardError; end
         class RPSRegisterTypeError < StandardError; end
+        class InvalidParamError    < StandardError; end
     end
 end
