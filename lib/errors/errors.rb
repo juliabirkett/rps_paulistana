2a3ba8abcca9971ac55a9eaced52519b2360a3c2
Júlia Marczak Birkett <julia.birkett@MBP-de-123.home>
2016-01-29 17:52:52 -0200

    Adding errors classes.

--- lib/errors/errors.rb
+++ lib/errors/errors.rb
@@ -3,5 +3,10 @@ module NFE
         class ParamClassError      < StandardError; end
         class RPSRegisterTypeError < StandardError; end
         class InvalidParamError    < StandardError; end
+        class LayoutVersionError   < StandardError; end
+        class RPSTypeError         < StandardError; end
+        class RPSStatusError       < StandardError; end
+        class ISSByError           < StandardError; end
+        class TakerTypeError       < StandardError; end
     end
 end
