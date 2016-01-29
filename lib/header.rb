1405c038781c24c76bd7bddea1406c2566f21131
Júlia Marczak Birkett <julia.birkett@MBP-de-123.home>
2016-01-29 17:53:17 -0200

    Header class first implementation.

--- lib/header.rb
+++ lib/header.rb
@@ -1,5 +1,11 @@
 module NFE
     class Header < Register
+        attr_reader :fields
+
         REQUIRED_FIELDS = []
+
+        def initialize fields
+            super 1
+        end
     end
 end
