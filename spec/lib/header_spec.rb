ed6321c2a088e678ba302f3f9dc3fe9039dc5fba
Júlia Marczak Birkett <julia.birkett@MBP-de-123.home>
2016-01-29 17:55:14 -0200

    Header class specs for #new method.

--- spec/lib/header_spec.rb
+++ spec/lib/header_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe NFE::Header do
+    describe "#new" do
+        context "passing parameter(s)" do
+            it "raises an ArgumentError" do
+                expect{ NFE::Header.new "teste" }.to raise_error ArgumentError
+            end
+        end
+
+        it "returns an NFE::Header object" do
+            expect(NFE::Header.new).to be_an NFE::Header
+        end
+    end
     # describe "#valid?" do
     #     it "return false if the register does not contain required fields" do
-    #         @register = NFE::Register.new "2"
-    #         expect(@register.valid?).to be false
+    #         expect((NFE::Header.new).valid?).to be false
     #     end
     #     it "return true if the register contains required fields"
     # end
