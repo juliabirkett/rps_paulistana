d6079d158f540ad1647e9e79a620ce5533e6760e
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-01 17:59:03 -0200

    Header class implementation

--- lib/header.rb
+++ lib/header.rb
@@ -1,11 +1,10 @@
 module NFE
     class Header < Register
-        attr_reader :fields
+        REQUIRED_FIELDS = [:layout_version, :municipal_registration, :date]
 
-        REQUIRED_FIELDS = []
-
-        def initialize fields
+        def initialize
             super 1
         end
+
     end
 end
