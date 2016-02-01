1d562726e1a379b96503d481196f674ed5df402f
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-01 17:59:43 -0200

    Header spec

--- spec/lib/header_spec.rb
+++ spec/lib/header_spec.rb
@@ -12,10 +12,36 @@ describe NFE::Header do
             expect(NFE::Header.new).to be_an NFE::Header
         end
     end
-    # describe "#valid?" do
-    #     it "return false if the register does not contain required fields" do
-    #         expect((NFE::Header.new).valid?).to be false
-    #     end
-    #     it "return true if the register contains required fields"
-    # end
+
+    describe "#<<" do
+        context "with invalid parameter" do
+            it "raises an InvalidParamError" do
+                expect{ NFE::Header::new << "teste" }.to raise_error NFE::Errors::InvalidParamError
+            end
+        end
+
+        context "with valid parameter and valid fields" do
+            it "returns a populated Hash of RPSField's" do
+                expect((NFE::Header::new << {layout_version: "002", municipal_registration: "48815446"}).empty?).to be false
+                expect(NFE::Header::new << {layout_version: "002", municipal_registration: "municipal_registration"})         .to be_an Hash
+            end
+        end
+
+        context "with valid parameter, but invalid fields" do
+            it "returns an empty Hash" do
+                expect((NFE::Header::new << {teste: "idk", nonexistent: "aa"}).empty?).to be true
+            end
+        end
+    end
+
+    describe "#valid?" do
+        it "return whether the register contains required fields" do
+            header = NFE::Header.new
+            expect(header.valid?).to be false
+            header << {layout_version: "002", municipal_registration: "48815446"}
+            expect(header.valid?).to be false
+            header << {layout_version: "002", municipal_registration: "48815446", date: "20160201"}
+            expect(header.valid?).to be true
+        end
+    end
 end
