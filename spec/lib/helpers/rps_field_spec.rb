1c8dfa9293d2908d8b65fbb881a25e2563e88ec3
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-02 17:51:07 -0200

    RPSField spec.

--- spec/lib/helpers/rps_field_spec.rb
+++ spec/lib/helpers/rps_field_spec.rb
@@ -55,64 +55,74 @@ describe NFE::Helper::RPSField do
     end
 
     describe "#valid?" do
-        it "returns whether a RPSField is valid" do
-            expect((NFE::Helper::RPSField.new :uf, "SP").valid?)                                .to be true
-            expect((NFE::Helper::RPSField.new :iss_by, "2").valid?)                             .to be true
-            expect((NFE::Helper::RPSField.new :rps_type, "RPS").valid?)                         .to be true
-            expect((NFE::Helper::RPSField.new :rps_status, "T").valid?)                         .to be true
-            expect((NFE::Helper::RPSField.new :taker_type, "1").valid?)                         .to be true
-            expect((NFE::Helper::RPSField.new :aliquot, "0500").valid?)                         .to be true
-            expect((NFE::Helper::RPSField.new :date, "20160101").valid?)                        .to be true
-            expect((NFE::Helper::RPSField.new :city, "SAO PAULO").valid?)                       .to be true
-            expect((NFE::Helper::RPSField.new :address_type, "AV").valid?)                      .to be true
-            expect((NFE::Helper::RPSField.new :rps_serial, "12345").valid?)                     .to be true
-            expect((NFE::Helper::RPSField.new :address_type, "RUA").valid?)                     .to be true
-            expect((NFE::Helper::RPSField.new :address, "MARANHAO").valid?)                     .to be true
-            expect((NFE::Helper::RPSField.new :complement, "FUNDOS").valid?)                    .to be true
-            expect((NFE::Helper::RPSField.new :zip_code, "01225001").valid?)                    .to be true
-            expect((NFE::Helper::RPSField.new :layout_version, "002").valid?)                   .to be true
-            expect((NFE::Helper::RPSField.new :address_number, "600").valid?)                   .to be true
-            expect((NFE::Helper::RPSField.new :service_code, "06157").valid?)                   .to be true
-            expect((NFE::Helper::RPSField.new :district, "HIGIENOPOLIS").valid?)                .to be true
-            expect((NFE::Helper::RPSField.new :amount, "000000000050085").valid?)               .to be true
-            expect((NFE::Helper::RPSField.new :city_ibge_code, "1234567").valid?)               .to be true
-            expect((NFE::Helper::RPSField.new :rps_number, "000000000001").valid?)              .to be true
-            expect((NFE::Helper::RPSField.new :tributary_source, "idontknow").valid?)           .to be true
-            expect((NFE::Helper::RPSField.new :tributary_percentage, "60023").valid?)           .to be true
-            expect((NFE::Helper::RPSField.new :taker_document, "43896729837").valid?)           .to be true
-            expect((NFE::Helper::RPSField.new :total_detail_lines, "0000005").valid?)           .to be true
-            expect((NFE::Helper::RPSField.new :municipal_registration, "48815446").valid?)      .to be true
-            expect((NFE::Helper::RPSField.new :taker_name, "FERNANDO FARIA DE SOUZA").valid?)   .to be true
-            expect((NFE::Helper::RPSField.new :taker_email, "julia.birkett@99motos.com").valid?).to be true
-            expect((NFE::Helper::RPSField.new :service_description, "Agenciamento de motoboy realizados através da plataforma 99motos").valid?).to be true
+        context "with valid field name" do
+            it "returns whether a RPSField value is valid" do
+                expect((NFE::Helper::RPSField.new :uf, "SP").valid?)                                .to be true
+                expect((NFE::Helper::RPSField.new :iss_by, "2").valid?)                             .to be true
+                expect((NFE::Helper::RPSField.new :rps_type, "RPS").valid?)                         .to be true
+                expect((NFE::Helper::RPSField.new :rps_status, "T").valid?)                         .to be true
+                expect((NFE::Helper::RPSField.new :taker_type, "1").valid?)                         .to be true
+                expect((NFE::Helper::RPSField.new :aliquot, "0500").valid?)                         .to be true
+                expect((NFE::Helper::RPSField.new :start_date, "20160101").valid?)                  .to be true
+                expect((NFE::Helper::RPSField.new :end_date, "20160101").valid?)                    .to be true
+                expect((NFE::Helper::RPSField.new :city, "SAO PAULO").valid?)                       .to be true
+                expect((NFE::Helper::RPSField.new :address_type, "AV").valid?)                      .to be true
+                expect((NFE::Helper::RPSField.new :rps_serial, "12345").valid?)                     .to be true
+                expect((NFE::Helper::RPSField.new :address_type, "RUA").valid?)                     .to be true
+                expect((NFE::Helper::RPSField.new :address, "MARANHAO").valid?)                     .to be true
+                expect((NFE::Helper::RPSField.new :complement, "FUNDOS").valid?)                    .to be true
+                expect((NFE::Helper::RPSField.new :zip_code, "01225001").valid?)                    .to be true
+                expect((NFE::Helper::RPSField.new :layout_version, "002").valid?)                   .to be true
+                expect((NFE::Helper::RPSField.new :address_number, "600").valid?)                   .to be true
+                expect((NFE::Helper::RPSField.new :service_code, "06157").valid?)                   .to be true
+                expect((NFE::Helper::RPSField.new :district, "HIGIENOPOLIS").valid?)                .to be true
+                expect((NFE::Helper::RPSField.new :amount, "000000000050085").valid?)               .to be true
+                expect((NFE::Helper::RPSField.new :city_ibge_code, "1234567").valid?)               .to be true
+                expect((NFE::Helper::RPSField.new :rps_number, "000000000001").valid?)              .to be true
+                expect((NFE::Helper::RPSField.new :tributary_source, "idontknow").valid?)           .to be true
+                expect((NFE::Helper::RPSField.new :tributary_percentage, "60023").valid?)           .to be true
+                expect((NFE::Helper::RPSField.new :taker_document, "43896729837").valid?)           .to be true
+                expect((NFE::Helper::RPSField.new :total_detail_lines, "0000005").valid?)           .to be true
+                expect((NFE::Helper::RPSField.new :municipal_registration, "48815446").valid?)      .to be true
+                expect((NFE::Helper::RPSField.new :taker_name, "FERNANDO FARIA DE SOUZA").valid?)   .to be true
+                expect((NFE::Helper::RPSField.new :taker_email, "julia.birkett@99motos.com").valid?).to be true
+                expect((NFE::Helper::RPSField.new :service_description, "Agenciamento de motoboy realizados através da plataforma 99motos").valid?).to be true
 
-            expect((NFE::Helper::RPSField.new :uf, "SPRJ").valid?)                              .to be false
-            expect((NFE::Helper::RPSField.new :iss_by, "J").valid?)                             .to be false
-            expect((NFE::Helper::RPSField.new :rps_type, "ASLKAAA").valid?)                     .to be false
-            expect((NFE::Helper::RPSField.new :rps_status, "LALA").valid?)                      .to be false
-            expect((NFE::Helper::RPSField.new :taker_type, "444").valid?)                       .to be false
-            expect((NFE::Helper::RPSField.new :aliquot, "la").valid?)                           .to be false
-            expect((NFE::Helper::RPSField.new :date, "201601012").valid?)                       .to be false
-            expect((NFE::Helper::RPSField.new :city, "SAO PAULO!").valid?)                      .to be false
-            expect((NFE::Helper::RPSField.new :address_type, "AVAAA").valid?)                   .to be false
-            expect((NFE::Helper::RPSField.new :rps_serial, "AKSSDASF").valid?)                  .to be false
-            expect((NFE::Helper::RPSField.new :address, "MARANHA@@@O").valid?)                  .to be false
-            expect((NFE::Helper::RPSField.new :complement, "$").valid?)                         .to be false
-            expect((NFE::Helper::RPSField.new :zip_code, "021225001").valid?)                   .to be false
-            expect((NFE::Helper::RPSField.new :layout_version, "0s02").valid?)                  .to be false
-            expect((NFE::Helper::RPSField.new :address_number, "sa600sa600sa600").valid?)       .to be false
-            expect((NFE::Helper::RPSField.new :service_code, "asdd").valid?)                    .to be false
-            expect((NFE::Helper::RPSField.new :district, "HIGIENOP@@OLIS").valid?)              .to be false
-            expect((NFE::Helper::RPSField.new :amount, "000000000050a085").valid?)              .to be false
-            expect((NFE::Helper::RPSField.new :city_ibge_code, "åsdas").valid?)                 .to be false
-            expect((NFE::Helper::RPSField.new :rps_number, "000000000@001").valid?)             .to be false
-            expect((NFE::Helper::RPSField.new :tributary_source, "idontknow!!").valid?)         .to be false
-            expect((NFE::Helper::RPSField.new :tributary_percentage, "60023232").valid?)        .to be false
-            expect((NFE::Helper::RPSField.new :taker_document, "438967298371212332").valid?)    .to be false
-            expect((NFE::Helper::RPSField.new :total_detail_lines, "0000005!").valid?)          .to be false
-            expect((NFE::Helper::RPSField.new :municipal_registration, "4881aaa5446").valid?)   .to be false
-            expect((NFE::Helper::RPSField.new :taker_name, "FERNANDO FARIA DE SOUZ!!@A").valid?).to be false
-            expect((NFE::Helper::RPSField.new :service_description, "Agenciamento !de motoboy realizados através da plataforma 99motos").valid?).to be false
+                expect((NFE::Helper::RPSField.new :uf, "SPRJ").valid?)                              .to be false
+                expect((NFE::Helper::RPSField.new :iss_by, "J").valid?)                             .to be false
+                expect((NFE::Helper::RPSField.new :rps_type, "ASLKAAA").valid?)                     .to be false
+                expect((NFE::Helper::RPSField.new :rps_status, "LALA").valid?)                      .to be false
+                expect((NFE::Helper::RPSField.new :taker_type, "444").valid?)                       .to be false
+                expect((NFE::Helper::RPSField.new :aliquot, "la").valid?)                           .to be false
+                expect((NFE::Helper::RPSField.new :start_date, "201601012").valid?)                 .to be false
+                expect((NFE::Helper::RPSField.new :end_date, "201601012").valid?)                   .to be false
+                expect((NFE::Helper::RPSField.new :city, "SAO PAULO!").valid?)                      .to be false
+                expect((NFE::Helper::RPSField.new :address_type, "AVAAA").valid?)                   .to be false
+                expect((NFE::Helper::RPSField.new :rps_serial, "AKSSDASF").valid?)                  .to be false
+                expect((NFE::Helper::RPSField.new :address, "MARANHA@@@O").valid?)                  .to be false
+                expect((NFE::Helper::RPSField.new :complement, "$").valid?)                         .to be false
+                expect((NFE::Helper::RPSField.new :zip_code, "021225001").valid?)                   .to be false
+                expect((NFE::Helper::RPSField.new :layout_version, "0s02").valid?)                  .to be false
+                expect((NFE::Helper::RPSField.new :address_number, "sa600sa600sa600").valid?)       .to be false
+                expect((NFE::Helper::RPSField.new :service_code, "asdd").valid?)                    .to be false
+                expect((NFE::Helper::RPSField.new :district, "HIGIENOP@@OLIS").valid?)              .to be false
+                expect((NFE::Helper::RPSField.new :amount, "000000000050a085").valid?)              .to be false
+                expect((NFE::Helper::RPSField.new :city_ibge_code, "åsdas").valid?)                 .to be false
+                expect((NFE::Helper::RPSField.new :rps_number, "000000000@001").valid?)             .to be false
+                expect((NFE::Helper::RPSField.new :tributary_source, "idontknow!!").valid?)         .to be false
+                expect((NFE::Helper::RPSField.new :tributary_percentage, "60023232").valid?)        .to be false
+                expect((NFE::Helper::RPSField.new :taker_document, "438967298371212332").valid?)    .to be false
+                expect((NFE::Helper::RPSField.new :total_detail_lines, "0000005!").valid?)          .to be false
+                expect((NFE::Helper::RPSField.new :municipal_registration, "4881aaa5446").valid?)   .to be false
+                expect((NFE::Helper::RPSField.new :taker_name, "FERNANDO FARIA DE SOUZ!!@A").valid?).to be false
+                expect((NFE::Helper::RPSField.new :service_description, "Agenciamento !de motoboy realizados através da plataforma 99motos").valid?).to be false
+            end
+        end
+
+        context "with invalid field name" do
+            it "raises an NonExistentFieldError" do
+                expect{(NFE::Helper::RPSField.new :teste, "whatever").valid?}.to raise_error NFE::Errors::NonExistentFieldError
+            end
         end
     end
 end
