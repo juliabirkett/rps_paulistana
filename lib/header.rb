48a434a1a6a7ee6786d20f4b9f4cc23fea62f503
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-02 17:47:32 -0200

    Adding DEFAULTS values to Header class

--- lib/header.rb
+++ lib/header.rb
@@ -1,10 +1,14 @@
 module NFE
     class Header < Register
         REQUIRED_FIELDS = [:layout_version, :municipal_registration, :start_date, :end_date]
+        DEFAULTS = {
+            layout_version: "002",
+            start_date: Time.now.to_date.strftime("%Y%m%d"),
+            end_date:   Time.now.to_date.strftime("%Y%m%d")
+        }
 
         def initialize
             super 1
         end
-
     end
 end
