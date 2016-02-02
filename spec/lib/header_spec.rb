78f3e377374ab6af9dda9713dc1638936cb8e17e
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-02 17:51:15 -0200

    Header spec.

--- spec/lib/header_spec.rb
+++ spec/lib/header_spec.rb
@@ -4,7 +4,7 @@ describe NFE::Header do
     describe "#new" do
         context "passing parameter(s)" do
             it "raises an ArgumentError" do
-                expect{ NFE::Header.new "teste" }.to raise_error ArgumentError
+                expect{NFE::Header.new "teste"}.to raise_error ArgumentError
             end
         end
 
@@ -16,32 +16,40 @@ describe NFE::Header do
     describe "#<<" do
         context "with invalid parameter" do
             it "raises an InvalidParamError" do
-                expect{ NFE::Header::new << "teste" }.to raise_error NFE::Errors::InvalidParamError
+                expect{NFE::Header::new << "teste"}.to raise_error NFE::Errors::InvalidParamError
             end
         end
 
-        context "with valid parameter and valid fields" do
-            it "returns a populated Hash of RPSField's" do
-                expect((NFE::Header::new << {layout_version: "002", municipal_registration: "48815446"}).empty?).to be false
-                expect(NFE::Header::new << {layout_version: "002", municipal_registration: "municipal_registration"})         .to be_an Hash
+        context "with valid parameter" do
+            context "with nonexistent field" do
+                it "raises an NonExistentFieldError" do
+                    expect{NFE::Header::new << {teste: "idk", nonexistent: "aa"}}.to raise_error NFE::Errors::NonExistentFieldError
+                end
             end
-        end
 
-        context "with valid parameter, but invalid fields" do
-            it "returns an empty Hash" do
-                expect((NFE::Header::new << {teste: "idk", nonexistent: "aa"}).empty?).to be true
+            context "with invalid field" do
+                it "raises an InvalidFieldError" do
+                    expect{(NFE::Header::new << {layout_version: "01", municipal_registration: "48815446"})}.to raise_error NFE::Errors::InvalidFieldError
+                end
+            end
+
+            context "with valid fields" do
+                it "returns a populated Hash of field name/value" do
+                    expect((NFE::Header::new << {layout_version: "002", municipal_registration: "48815446"}).empty?).to be false
+                    expect( NFE::Header::new << {layout_version: "002", municipal_registration: "48815446"})        .to be_an Hash
+                end
             end
         end
     end
 
-    describe "#valid?" do
-        it "return whether the register contains required fields" do
-            header = NFE::Header.new
-            expect(header.valid?).to be false
-            header << {layout_version: "002", municipal_registration: "48815446"}
-            expect(header.valid?).to be false
-            header << {layout_version: "002", municipal_registration: "48815446", date: "20160201"}
-            expect(header.valid?).to be true
-        end
-    end
+    # describe "#valid?" do
+    #     it "return whether the register contains required fields" do
+    #         header = NFE::Header.new
+    #         expect(header.valid?).to be false
+    #         header << {layout_version: "002", municipal_registration: "48815446"}
+    #         expect(header.valid?).to be false
+    #         header << {layout_version: "002", municipal_registration: "48815446", start_date: "20160201", end_date: "20160201"}
+    #         expect(header.valid?).to be true
+    #     end
+    # end
 end
