1833d2b5d0cf2846c72a3a17c4d85814e16f7690
Júlia Marczak Birkett <julia.birkett@MacBook-Pro-de-123.local>
2016-02-05 16:42:02 -0200

    Fixing add_footer method and changing its name to set_footer, since the user won't pass any parameters.

--- lib/rps.rb
+++ lib/rps.rb
@@ -20,8 +20,15 @@ module NFE
             return detail.valid?
         end
 
-        def add_footer fields
-            @footer << fields
+        def set_footer
+            total_amount     = 0
+            total_tax_amount = 0
+            @details.each do |detail|
+                total_amount     += detail.fields[:amount].to_i
+                total_tax_amount += detail.fields[:tax_amount].to_i
+            end
+
+            @footer << {total_detail_lines: @details.count.to_s, total_amount: total_amount.to_s, total_tax_amount: total_tax_amount.to_s}
             return @footer.valid?
         end
 
