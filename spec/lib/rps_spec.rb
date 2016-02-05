ce0977d633248c6885b04db225235d97d0857f4f
Júlia Marczak Birkett <julia.birkett@MBP-de-123.home>
2016-02-04 23:33:53 -0200

    Adding #add_detail method specs to RPS class.

--- spec/lib/rps_spec.rb
+++ spec/lib/rps_spec.rb
@@ -44,6 +44,41 @@ describe NFE::RPS do
                     expect((NFE::RPS.new).add_header({municipal_registration: "12345678"})).to be true
                     expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", start_date: "20161220"})).to be true
                     expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", start_date: "20161220", end_date: "20162123"})).to be true
+
+    describe "#add_detail" do
+        context "with invalid parameter" do
+            it "raise InvalidParamError" do
+                expect{(NFE::RPS.new).add_detail "teste"}.to raise_error NFE::Errors::InvalidParamError
+            end
+        end
+
+        context "with valid parameter" do
+            context "with nonexistent field name" do
+                it "raises NonExistentFieldError" do
+                    expect{(NFE::RPS.new).add_detail({troubles: "whatever"})}              .to raise_error NFE::Errors::NonExistentFieldError
+                    expect{(NFE::RPS.new).add_detail({municipal_registration: "12345678"})}.to raise_error NFE::Errors::NonExistentFieldError
+                    expect{(NFE::RPS.new).add_detail({layout_version: "003"})}.to raise_error NFE::Errors::NonExistentFieldError
+                end
+            end
+
+            context "with invalid field value" do
+                it "raises an Error" do
+                    expect{(NFE::RPS.new).add_detail({rps_type: "003"})}.to raise_error NFE::Errors::RPSTypeError
+                    expect{(NFE::RPS.new).add_detail({rps_status: "I"})}.to raise_error NFE::Errors::RPSStatusError
+                    expect{(NFE::RPS.new).add_detail({iss_by: "5"})}    .to raise_error NFE::Errors::ISSByError
+                    expect{(NFE::RPS.new).add_detail({taker_type: "5"})}.to raise_error NFE::Errors::TakerTypeError
+                end
+            end
+
+            context "with valid field" do
+                it "returns whether the header was added" do
+                    expect{(NFE::RPS.new).add_detail({layout_version: "002"})}.to_not raise_error
+                    expect((NFE::RPS.new).add_detail({layout_version: "002"})).to be false
+                    expect((NFE::RPS.new).add_detail({start_date: "20161220"})).to be false
+                    expect((NFE::RPS.new).add_detail({end_date: "20161220"})).to be false
+                    expect((NFE::RPS.new).add_detail({municipal_registration: "12345678"})).to be true
+                    expect((NFE::RPS.new).add_detail({layout_version: "002", municipal_registration: "12345678", start_date: "20161220"})).to be true
+                    expect((NFE::RPS.new).add_detail({layout_version: "002", municipal_registration: "12345678", start_date: "20161220", end_date: "20162123"})).to be true
                 end
             end
         end
