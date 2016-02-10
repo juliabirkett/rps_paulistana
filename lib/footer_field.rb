7b6e922df37be03706e7fc3eeb229fa4c4417463
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-10 18:36:20 -0200

    Deleting specific field classes.

--- lib/footer_field.rb
+++
@@ -1,9 +0,0 @@
-module NFE
-    class FooterField < RPSField
-        VALID_NAMES = [
-            :total_detail_lines,
-            :total_amount,
-            :total_tax_amount,
-        ]
-    end
-end
