bdd2ce503bb64df37768ef1d0da6e41050bb115c
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-15 18:32:23 -0200

    RPS class.

--- lib/rps.rb
+++ lib/rps.rb
@@ -10,26 +10,23 @@ module NFE
 
         def add_header fields
             @header << fields
-            return @header.valid?
         end
 
         def add_detail fields
             detail = Detail.new
             detail << fields
             @details << detail  if detail.valid?
-            return detail.valid?
         end
 
         def set_footer
             total_amount     = 0
             total_tax_amount = 0
             @details.each do |detail|
-                total_amount     += detail.fields[:amount].to_i
-                total_tax_amount += detail.fields[:tax_amount].to_i
+                total_amount     += detail.to_hash[:amount].to_i
+                total_tax_amount += detail.to_hash[:tax_amount].to_i
             end
 
             @footer << {total_detail_lines: @details.count.to_s, total_amount: total_amount.to_s, total_tax_amount: total_tax_amount.to_s}
-            return @footer.valid?
         end
 
         def to_s
