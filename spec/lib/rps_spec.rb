83587914f2716518e39f906b2c592c47fa0edbd2
Julia Birkett <julia@Julia-Birkett-Mac-2.local>
2016-02-18 18:27:21 -0200

    Finishing RPS class specs.

--- spec/lib/rps_spec.rb
+++ spec/lib/rps_spec.rb
@@ -35,12 +35,20 @@ describe NFE::RPS do
                 end
             end
 
-            context "with valid field" do
-                it "returns whether the header was added" do
-                    expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", \
-                        start_date: "20161220"}).empty?).to be false
-                    expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", \
-                        start_date: "20161220", end_date: "20161223"}).empty?).to be false
+            context "with valid fields" do
+                context "when passing all required fields (valid Header)" do
+                    it "returns whether the header was added" do
+                        expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", \
+                            start_date: "20161220"}).empty?).to be false
+                        expect((NFE::RPS.new).add_header({layout_version: "002", municipal_registration: "12345678", \
+                            start_date: "20161220", end_date: "20161223"}).empty?).to be false
+                    end
+                end
+
+                context "when missing required fields (invalid Header)" do
+                    it "raises an InvalidHeaderError" do
+                        expect{(NFE::RPS.new).add_header({start_date: "20160212"})}.to raise_error NFE::Errors::InvalidHeaderError
+                    end
                 end
             end
         end
@@ -71,24 +79,32 @@ describe NFE::RPS do
             end
 
             context "with valid field" do
-                it "returns whether the detail was added" do
-                    true_hash = {
-                        rps_number: "1",
-                        amount: "1000",
-                        tax_amount: "0",
-                        service_code: "12345",
-                        aliquot: "0",
-                        iss_by: "1",
-                        taker_type: "1",
-                        taker_document: "43896729837",
-                        service_description: "Agenciamento de motoboy realizados através da plataforma 99motos"
-                    }
-                    false_hash = {
-                        rps_number: "2",
-                        amount: "10"
-                    }
-                    # expect((NFE::RPS.new).add_detail(true_hash).empty?) .to be false
-                    # expect((NFE::RPS.new).add_detail(false_hash).empty?).to be false
+                context "when passing all required fields (valid Detail)" do
+                    it "returns whether the detail was added" do
+                        true_hash = {
+                            rps_number: "1",
+                            amount: "1000",
+                            tax_amount: "0",
+                            service_code: "12345",
+                            aliquot: "0",
+                            iss_by: "1",
+                            taker_type: "1",
+                            taker_document: "43896729837",
+                            rps_status: "T",
+                            service_description: "Agenciamento de motoboy realizados através da plataforma 99motos"
+                        }
+                        expect((NFE::RPS.new).add_detail(true_hash).empty?).to be false
+                    end
+                end
+
+                context "when missing required fields (invalid Detail)" do
+                    it "raises an InvalidDetailError" do
+                        false_hash = {
+                            rps_number: "2",
+                            amount: "10"
+                        }
+                        expect{(NFE::RPS.new).add_detail(false_hash)}.to raise_error NFE::Errors::InvalidDetailError
+                    end
                 end
             end
         end
@@ -111,6 +127,62 @@ describe NFE::RPS do
     end
 
     describe "#to_s" do
+        context "with valid RPS" do
+            it "returns a String containing the registers" do
+                rps = NFE::RPS.new
+                rps.add_header(header = {municipal_registration: "46923764"})
+                rps.add_detail({
+                    rps_number: "1",
+                    rps_status: "T",
+                    amount: "100",
+                    tax_amount: "0",
+                    service_code: "06298",
+                    aliquot: "0500",
+                    iss_by: "2",
+                    taker_type: "1",
+                    taker_document: "43896729837",
+                    service_description: "Agenciamento de motoboy realizados através da plataforma 99motos"
+                })
+                rps.set_footer
+                expect(rps.to_s).to be_an String
+            end
+        end
 
+        context "with invalid RPS" do
+            it "raises an InvalidRPSError" do
+                rps = NFE::RPS.new
+                rps.add_header(header = {municipal_registration: "46923764"})
+                expect{rps.to_s}.to raise_error NFE::Errors::InvalidRPSError
+            end
+        end
+    end
+
+    describe "#save_to_file" do
+        context "when saved file" do
+            it "returns a String containing the filename" do
+                rps = NFE::RPS.new
+                rps.add_header(header = {municipal_registration: "46923764"})
+                rps.add_detail({
+                    rps_number: "1",
+                    rps_status: "T",
+                    amount: "100",
+                    tax_amount: "0",
+                    service_code: "06298",
+                    aliquot: "0500",
+                    iss_by: "2",
+                    taker_type: "1",
+                    taker_document: "43896729837",
+                    service_description: "Agenciamento de motoboy realizados através da plataforma 99motos"
+                })
+                rps.set_footer
+                expect(rps.save_to_file).to be_an String
+            end
+        end
+
+        context "when did not save file" do
+            it "raises an Error" do
+                expect{(NFE::RPS.new).save_to_file ""}.to raise_error
+            end
+        end
     end
 end
